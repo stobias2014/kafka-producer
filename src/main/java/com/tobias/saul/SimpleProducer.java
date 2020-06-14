e50d36fd7f9b89acc366d0cf307ebf2c11442fe5
Saul Tobias <saul.tobias@fdmgroup.com>
2020-06-13 23:34:24 -0500

    added kafka producer

--- src/main/java/com/tobias/saul/SimpleProducer.java
+++ src/main/java/com/tobias/saul/SimpleProducer.java
@@ -1,9 +1,42 @@
 package com.tobias.saul;
 
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
 public class SimpleProducer {
-	
+
 	public static void main(String[] args) {
+		String topicName = "topic";
+
+		Properties props = new Properties();
+
+		props.put("bootstrap.servers", 9002);
+
+		props.put("acks", "all");
+
+		props.put("retries", 0);
+
+		props.put("batch.size", 16384);
+
+		props.put("linger.ms", 1);
+
+		props.put("buffer.memory", 33554432);
+
+		props.put("key.serializer", "org.apache.kafka.common.serializa-tion.StringSerializer");
+
+		props.put("value.serializer", "org.apache.kafka.common.serializa-tion.StringSerializer");
+
+		Producer<String, String> producer = new KafkaProducer<String, String>(props);
+
+		for (int i = 0; i < 10; i++) {
+			producer.send(new ProducerRecord<String, String>(topicName, Integer.toString(i), Integer.toString(i)));
+			System.out.println("Message successfully sent");	
+		}
 		
+		producer.close();
 	}
 
 }
